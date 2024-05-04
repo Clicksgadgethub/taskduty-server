669df53658f005a0a98642b48fad113a61a1aa16
clicksgadgethub <obinnaonyido1@gmail.com>
2024-05-04 14:37:00 +0200

    installed cors

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const mongoose = require("mongoose")
 const connect = require('./config/db')
 const userRouter = require('./routes/userRoute')
 const taskRouter = require('./routes/taskRoute')
+const cors = require("cors")
 
 
 // middlewares 
 app.use(express.json());
+app.use(cors())
 
 
 // API's
